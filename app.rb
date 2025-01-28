82177b2f297e9beb43d411fe04146f2fabf81df5
julialuo02 <121737951+julialuo02@users.noreply.github.com>
2025-01-28 14:25:41 +0000

    finished

--- app.rb
+++ app.rb
@@ -1,44 +1,61 @@
 require "sinatra"
 require "sinatra/reloader"
 
+# Redirect root to addition calculator
 get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
-  @first_num = params.fetch("first_num").to_f
+  @first_num = params[:first_num].to_f
+  @second_num = params[:second_num].to_f
 
   @result = @first_num + @second_num
   erb(:add_result)
 end
 
+# Subtraction Form
 get("/subtract") do
-  erb(:sub_form)
+  erb(:subtraction_form)
 end
 
 get("/wizard_subtract") do
-  @first_num = params.fetch("first_num").to_f
-  @second_num = params.fetch("second_num").to_f
+  @first_num = params[:first_num].to_f
+  @second_num = params[:second_num].to_f
 
+  @result = @first_num - @second_num
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params[:first_num].to_f
+  @second_num = params[:second_num].to_f
+
+  @result = @first_num * @second_num
   erb(:mult_result)
 end
 
-get("/divide") do
-  erb(:div_form)
+get "/divide" do
+  erb :divform
 end
 
-get("/wizard_divide") do
-  erb(:div_result)
+get "/wizard_divide" do
+  @first_num = params[:first_num].to_f
+  @second_num = params[:second_num].to_f
+
+  if @second_num.zero?
+    @result = "Undefined (division by zero is not allowed)"
+  else
+    @result = @first_num / @second_num
+  end
+
+  erb :div_result
 end
